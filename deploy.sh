ff5963b09ec3bcf20176fbfc92de72d40a0bd513
Otim Fredrick <fredrickot@gmail.com>
2020-05-12 09:10:08 +0300

    updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<REPO-NAME>.git master:gh-pages
+git push -f git@github.com:otim22/track_test_covid.git master:gh-pages
 
 cd -
