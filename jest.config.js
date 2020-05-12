7c3d80045d70deb1ec4af338ef95d978e96f5c5d
Abok Isaac <abokisaac@gmail.com>
2020-05-12 18:16:12 +0300

    chore: configure tests

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.vue$": "vue-jest"
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{js,vue}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+    "!**/coverage/**",
+    "!**/*.config.*",
+    "!src/main.js"
+  ]
+};
